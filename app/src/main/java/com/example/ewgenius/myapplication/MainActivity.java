edcedd8f20d74f74989eeaf111c755773b99b163
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-05-02 17:18:38 +0300

    mobile center sdk init

--- app/src/main/java/com/example/ewgenius/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ewgenius/myapplication/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.ewgenius.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileCenter.start(getApplication(), "8124512b-5296-4991-ab51-54af64c05e0f",
+                Analytics.class, Crashes.class);
     }
+
+    public void onClick(View v) {
+        throw new RuntimeException("This is a crash");
+    }
+
 }
